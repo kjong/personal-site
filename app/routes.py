7a1332d3fd8f9f15fdfe7ed65c1f6c0b43a2fc71
kjong <justinkim532@gmail.com>
2020-01-10 05:17:11 -0800

    add resume download functionality

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,12 @@
-from flask import render_template
+from flask import render_template, send_from_directory
 from app import app
 
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+
+@app.route('/resume')
+def resume():
+    return send_from_directory('uploads', 'Resume.pdf', mimetype='application/pdf', as_attachment=True)
